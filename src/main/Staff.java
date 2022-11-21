7806060e187cb3bd48b5e786ee60fcc22c838b28
ALLFON\Rajkumar.j <Rajkumar.j@excelacom.in>
2022-11-21 12:11:36 +0530

    added change

---
+++ src/main/Staff.java
@@ -0,0 +1,30 @@
+
+class Sample{
+    int id;
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+public class Staff {
+    public static void main(String ar[])
+    {
+        Sample s=new Sample();
+        s.setId(1);
+        s.setName("raj");
+        System.out.println("The id is "+s.getId()+" and the name is "+s.getName());
+    }
+}
